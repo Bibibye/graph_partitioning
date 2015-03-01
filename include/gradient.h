ff7e1d5dd8fac250871e54ce21887c41dae0c4d3
Bibibye <Bibibye@users.noreply.github.com>
2015-03-01 23:02:13 +0100

    Add missing define

--- include/gradient.h
+++ include/gradient.h
@@ -1,4 +1,9 @@
+#ifndef _GRADIENT_H_
+#define _GRADIENT_H_
+
 #include <neighborhood.h>
 #include <partitioning.h>
 
 struct solution* gradient(graph g, get_neighborhood f, valid v);
+
+#endif /* _GRADIENT_H_ */
