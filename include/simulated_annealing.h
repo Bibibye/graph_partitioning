f131139eb32080b57a0820615133a043e1c99f47
Bibibye <Bibibye@users.noreply.github.com>
2015-03-18 23:39:54 +0100

    Change initial temperature to 1000

--- include/simulated_annealing.h
+++ include/simulated_annealing.h
@@ -4,7 +4,7 @@
 #include <neighborhood.h>
 #include <partitioning.h>
 
-#define SIMULATED_ANNEALING_T_0 100
+#define SIMULATED_ANNEALING_T_0 1000
 #define SIMULATED_ANNEALING_NB_FAIL_MAX 10
 #define SIMULATED_ANNEALING_ACCEPTATION_PROBA 1000
 
