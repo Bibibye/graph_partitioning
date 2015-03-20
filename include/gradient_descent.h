63b0bede337cf9e5d4a300af0410631e4869af07
MrPantoufle <rapha.mac.mil@gmail.com>
2015-03-20 16:21:59 +0100

    jej

--- include/gradient_descent.h
+++ include/gradient_descent.h
@@ -4,7 +4,9 @@
 #include <neighborhood.h>
 #include <partitioning.h>
 
+#ifndef DESC_SEED
 #define DESC_SEED(v) (v/10.)
+#endif
 
 struct solution* gradient_descent(graph g, get_neighborhood f, valid v);
 
